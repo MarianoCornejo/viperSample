cb089a8a2d4114dee8990bba558cfb0ea0613634
Oscar Mariano Cornejo Herrera <>
2020-09-29 06:26:20 -0500

    First commit, adding proyect and gitignore

---
+++ viperSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viperSample
+//
+//  Created by Oscar Mariano Cornejo Herrera on 9/29/20.
+//  Copyright © 2020 Oscar Mariano Cornejo Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
