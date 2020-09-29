9a6abc97e2db483326a2defaeb4dbb019615b250
Oscar Mariano Cornejo Herrera <>
2020-09-29 08:27:28 -0500

    Adding more autolayout helpers , working on FeedTableViewCell

--- viperSample/Infrastructure/UIView+AutoLayout.swift
+++ viperSample/Infrastructure/UIView+AutoLayout.swift
@@ -8,11 +8,62 @@
 
 import UIKit
 
+enum LayoutCorner {
+    case topLeft(leftMargin: CGFloat, topMargin: CGFloat)
+    case topRight(leftMargin: CGFloat, topMargin: CGFloat)
+    case bottomLeft(leftMargin: CGFloat, topMargin: CGFloat)
+    case bottomRight(leftMargin: CGFloat, topMargin: CGFloat)
+}
+
+enum LayoutSides {
+    case left(margin: CGFloat)
+    case right(margin: CGFloat)
+    case top(margin: CGFloat)
+    case bottom(margin: CGFloat)
+}
+
 extension UIView {
     func expandTofitLayoutFromView(_ parentView: UIView) {
         leadingAnchor.constraint(equalTo: parentView.leadingAnchor).isActive = true
         trailingAnchor.constraint(equalTo: parentView.trailingAnchor).isActive = true
         topAnchor.constraint(equalTo: parentView.topAnchor).isActive = true
         bottomAnchor.constraint(equalTo: parentView.bottomAnchor).isActive = true
+    }
+    
+    func pinToCorner(_ layoutCorner: LayoutCorner, inParentView view: UIView) {
+        switch layoutCorner {
+        case .topLeft(let leftMargin, let topMargin):
+            leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftMargin).isActive = true
+            topAnchor.constraint(equalTo: view.topAnchor, constant: topMargin).isActive = true
+        case .topRight(let rightMargin, let topMargin):
+            trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: rightMargin).isActive = true
+            topAnchor.constraint(equalTo: view.topAnchor, constant: topMargin).isActive = true
+        case .bottomLeft(let leftMargin, let bottomMargin):
+            leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftMargin).isActive = true
+            bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: bottomMargin).isActive = true
+        case .bottomRight(let rightMargin, let bottomMargin):
+            trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: rightMargin).isActive = true
+            bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: bottomMargin).isActive = true
+        }
+    }
+    
+    func constraintToSize(_ size: CGSize) {
+        widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        heightAnchor.constraint(equalToConstant: size.height).isActive = true
+    }
+    
+    func expandToSides(_ sides: [LayoutSides], inParentView view: UIView) {
+        for side in sides {
+            switch side {
+            case .left(let margin):
+                leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin).isActive = true
+            case .right(let margin):
+                trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: margin).isActive = true
+            case .top(let margin):
+                topAnchor.constraint(equalTo: view.topAnchor, constant: margin).isActive = true
+            case .bottom(let margin):
+                bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: margin).isActive = true
+            }
+        }
     }
 }
--- viperSample/Modules/FeedsModule/FeedTableViewCell.swift
+++ viperSample/Modules/FeedsModule/FeedTableViewCell.swift
@@ -12,7 +12,12 @@ import UIKit
 class FeedTableViewCell: UITableViewCell {
     
     //MARK: - Properties
+    private var thumbnailImageView: UIImageView = UIImageView(frame: .zero)
+    private var authorLabel: UILabel = UILabel(frame: .zero)
+    private var entryDateLabel: UILabel = UILabel(frame: .zero)
     private var titleLabel: UILabel = UILabel(frame: .zero)
+    private var numberOfCommentsLabel: UILabel = UILabel(frame: .zero)
+    private var readStatusLabel: UILabel = UILabel(frame: .zero)
     
     //MARK: - Initializer
     init(reuseIdentifier: String) {
@@ -26,8 +31,26 @@ class FeedTableViewCell: UITableViewCell {
     
     //MARK: - Private
     private func setupView() {
+        addSubview(thumbnailImageView)
+        addSubview(authorLabel)
+        addSubview(entryDateLabel)
         addSubview(titleLabel)
+        addSubview(numberOfCommentsLabel)
+        addSubview(readStatusLabel)
+        
+        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
+        authorLabel.translatesAutoresizingMaskIntoConstraints = false
+        entryDateLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.expandTofitLayoutFromView(self)
+        numberOfCommentsLabel.translatesAutoresizingMaskIntoConstraints = false
+        readStatusLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        thumbnailImageView.backgroundColor = .black
+        thumbnailImageView.constraintToSize(CGSize(width: 30, height: 30))
+        thumbnailImageView.pinToCorner(.topLeft(leftMargin: 10, topMargin: 10), inParentView: self)
+        
+        authorLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 10).isActive = true
+        authorLabel.expandToSides([.top(margin: 10), .right(margin: 10)], inParentView: self)
+        authorLabel.text = "adfas"
     }
 }
